7dd82cf807b60bf5dd035cf8dd25ce6cdf8b3b42
priya-0307 <priyamathiyalakan03@gmail.com>
2025-06-21 17:22:42 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-yum update -y
-yum install -y httpd
+
+sudo apt-get update
+sudo apt-get install openjdk-11-jdk unzip -y
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+aws s3 cp s3://codebuild-demo-21st-june-2025/app.jar /home/ubuntu/app.jar  #make sure to change the bucket name here
