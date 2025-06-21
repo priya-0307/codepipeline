62ff9d80db9b580f8d5645f6b33837268c86d2ef
priya-0307 <priyamathiyalakan03@gmail.com>
2025-06-21 17:23:17 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-systemctl start httpd
-systemctl enable httpd
+
+# Navigate to the directory where the JAR file is located
+cd /home/ubuntu/
+
+# Start the Java application
+java -jar app.jar > /var/log/app.log 2>&1 &
